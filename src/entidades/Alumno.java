fa21192204f6358c65b94bb4d0b1ed7b23ad5a07
Estefania Cabrera <estefi.cabrera22@gmail.com>
2026-09-14 21:10:20 -0300

    paquete entidades
    Alumno
    Materia

---
+++ src/entidades/Alumno.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author estef
+ */
+public class Alumno {
+    private int legajo;
+    private String apellido;
+    private String nombre;
+    private HashSet<Materia> materias;
+
+    public Alumno(int legajo, String apellido, String nombre) {
+        this.legajo = legajo;
+        this.apellido = apellido;
+        this.nombre = nombre;
+        this.materias= new HashSet<>();
+    }
+
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public void setLegajo(int legajo) {
+        this.legajo = legajo;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean agregarMateria(Materia m){
+        return materias.add(m);  
+        }
+    
+    public int cantidadMateria(){
+        return materias.size();
+    }
+    
+}
